6bb1eef8044dce6506812ac3e1ffe66edd384cec
Oleksandr Nevierov <usefree@Oleksandrs-MBP.Dlink>
2018-08-01 16:55:03 +0300

    Add Check_ip_url.sh

---
+++ Check_ip_url.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "" ] || [ "$2" == "" ]
+then
+        echo "usage: Check_ip_url.sh url ip"
+        exit 0
+fi
+is_true=$(ping -c 1 $1 | awk '{print $3}' | awk 'FNR <=1 {print}' | tr -d "(,)")
+if [ "$is_true" == $2 ]
+then
+        echo "OK"
+        exit 0
+else
+        echo "Critical"
+        exit 2
+fi
